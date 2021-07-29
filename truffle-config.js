b74d904d699bf32dedd856b601d901264fd9ef3f
Yuma <requrd1989@gmail.com>
2021-07-29 15:54:30 +0900

    Add eth network settings

--- truffle-config.js
+++ truffle-config.js
@@ -1,19 +1,27 @@
 const path = require('path')
 const HDWalletProvider = require("@truffle/hdwallet-provider")
+const project_id = process.env["PROJECT_ID"]
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "vapp/src/contracts"),
   networks: {
     ropsten: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/6ee0e653d59b4964954a91709fd7abd9");
+      provider: function() {        
+        return new HDWalletProvider(process.env["MNEMONIC"], `https://ropsten.infura.io/v3/${project_id}`);
       },
       network_id: '3',
     },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(process.env["MNEMONIC"], `https://rinkeby.infura.io/v3/${project_id}`);
+      },
+      network_id: '4',
+    },
     test: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/");
+        return new HDWalletProvider(process.env["MNEMONIC"], "http://127.0.0.1:8545/");
       },
       network_id: '*',
     },
