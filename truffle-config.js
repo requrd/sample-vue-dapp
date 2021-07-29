9bf70a47202fa1e54cb17a796a50c210603b39b4
Yuma <requrd1989@gmail.com>
2021-07-29 14:48:11 +0900

    Add network settings

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,21 @@
 const path = require('path')
+const HDWalletProvider = require("@truffle/hdwallet-provider")
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "vapp/src/contracts"),
-
+  networks: {
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/6ee0e653d59b4964954a91709fd7abd9");
+      },
+      network_id: '3',
+    },
+    test: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/");
+      },
+      network_id: '*',
+    },
+  }
 };
